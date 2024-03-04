df669a1ed8ab7ae399de8feaf1d6bb0ff5c79270
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-04 16:16:10 +0100

    adding role switch controller for handling the user experience

---
+++ app/Http/Controllers/User/RoleSwitchController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Organizer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RoleSwitchController extends Controller
+{
+    public function switchToOrganizer()
+    {
+        $user = Auth::user();
+
+        if ($user) {
+            $user->roles()->sync([2]);
+            $username = $user->username;
+            $user_id = $user->id;
+            Organizer::create([
+                'username' => $username,
+                'user_id' => $user_id,
+            ]);
+        }
+
+        return redirect(route('organizer.index'));
+    }
+
+    public function swithchToUser()
+    {
+        $user = Auth::user();
+        if($user){
+            Organizer::where('user_id', $user->id)->delete();
+            $user->roles()->sync([3]);
+            return redirect()->intended('/');
+        }
+    }
+}
