d4c873da36d07c4c50fcea037f741195dd9593da
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-04 16:16:38 +0100

    organizer model

---
+++ app/Models/Organizer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Organizer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'username',
+        'event_id',
+        'user_id',
+    ];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function events()
+    {
+        return $this->hasMany(Event::class);
+    }
+}
