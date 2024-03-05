791e5b08b9b017f64dc15fdc84c15e49c90f7bc0
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-05 17:15:25 +0100

    event interacts with media

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Event extends Model
 {
-    use HasFactory;
+    use HasFactory,  InteractsWithMedia;
 
     public function users()
     {
