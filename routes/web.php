9fb001890a4dfffbe06ea195f439b7935a8ca3b8
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-05 10:27:27 +0100

    reset password logic and routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ForgotPasswordController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Organizer\OrganizerControlelr;
 use App\Http\Controllers\User\RoleSwitchController;
@@ -31,3 +32,16 @@ Route::get('/switchToOrganizer',[RoleSwitchController::class, 'switchToOrganizer
 
 // organizer Controller 
 Route::resource('organizer',OrganizerControlelr::class);
+//forgetPassword Controller
+
+// Show the form to request a password reset link
+Route::get('/forgot-password', [ForgotPasswordController::class, 'index'])->name('password.forget');
+
+// Handle the request to send a password reset link
+Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+
+// Show the form to reset the password
+Route::get('/reset-password/{token}', [ForgotPasswordController::class, 'showResetForm'])->name('password.reset');
+
+// Handle the password reset request
+Route::post('/reset-password', [ForgotPasswordController::class, 'reset'])->name('password.update');
