7f85d30ac858bed361f318a936968c5e3fbec4ab
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-06 18:43:37 +0100

    adding fileable in event

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -3,13 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\MediaLibrary\HasMedia;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Event extends Model
+class Event extends Model implements HasMedia
 {
     use HasFactory,  InteractsWithMedia;
 
+    protected $fillable = [
+        'title',
+        'description',
+        'start_date',
+        'end_date',
+        'location',
+        'available_seats',
+        'category_id',
+        'organizer_id',
+        'status',
+    ];
     public function users()
     {
         return $this->belongsToMany(User::class);
@@ -18,5 +30,9 @@ class Event extends Model
     public function categories()
     {
         return $this->belongsTo(Category::class);
+    }
+    public function organizer()
+    {
+        return $this->belongsTo(Organizer::class);
     }
 }
