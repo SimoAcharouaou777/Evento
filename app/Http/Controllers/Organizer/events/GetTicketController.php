9c6514afec72491943a60e09cc063687c7ec3a3a
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-08 14:11:42 +0100

    handling the logique of getaticket

--- app/Http/Controllers/Organizer/events/GetTicketController.php
+++ app/Http/Controllers/Organizer/events/GetTicketController.php
@@ -14,25 +14,34 @@ class GetTicketController extends Controller
     {
         $user = Auth::user(); 
         $event = Event::find($id);
-        $status = 'accepted';
+        $status = 'reserved';
 
         if ($user->events()->where('event_id', $event->id)->exists()) {
             return redirect()->back()->with('error', 'You have already reserved a ticket for this event.');
         }
         if ($event->type == 'auto') {
             $event->available_seats = $event->available_seats - 1;
-            $event['status'] = $status;
             $event->save(); 
             $user->events()->attach($event->id, ['status' => $status]);
             $pdf = Pdf::loadView('pdf', ['user'=>$user]);
             session()->flash('success', 'Ticket reservation successful!');
             return $pdf->download('invoice.pdf');
+          
         }else {
             $user->events()->attach($event->id, ['status' => 'pending']);
             session()->flash('info', 'Ticket reservation is pending approval.');
             return redirect()->back();
         }
-          
         
+        
+    }
+
+    public function downloadTicket(Event $event)
+    {
+        $user = Auth::user();
+        $status = 'reserved';
+        $event->users()->updateExistingPivot($user, ['status' => $status]);
+        $pdf = Pdf::loadView('pdf', ['user'=>$user]);
+        return $pdf->download('invoice.pdf');
     }
 }
