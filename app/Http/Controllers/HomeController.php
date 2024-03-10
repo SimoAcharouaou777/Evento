68fbc03c24d4a33c6f3574831afa2b359adcac86
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-10 16:34:01 +0000

    adding more details in homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -12,6 +13,15 @@ class HomeController extends Controller
         $events = Event::where('status', 'accepted')->latest()->take(9)->get();
         $mostReservedEvents = Event::where('status', 'accepted')->withCount('users')->orderBy('users_count', 'desc')->take(1)->get();
         $lastEvent = Event::where('status', 'accepted')->latest()->take(3)->get();
-        return view('home', compact('events','mostReservedEvents','lastEvent'));
+        $userCount = User::count();
+        $eventcount = Event::count();
+        $participantCount = User::whereHas('events', function ($query){
+            $query->where('event_user.status', 'reserved');
+        })->count();
+        $eventReserved = Event::whereHas('users', function ($query){
+            $query->where('event_user.status','reserved');
+        })->count();
+        $organizers = User::has('organizers')->get();
+        return view('home', compact('events','mostReservedEvents','lastEvent','userCount','eventcount','participantCount','eventReserved','organizers'));
     }
 }
