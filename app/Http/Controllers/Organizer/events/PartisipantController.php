7e9a50f98c6d5e2aaf25374a5da2db82a4c51f7a
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2024-03-14 10:50:59 +0000

    Add seat availability check and update request status

--- app/Http/Controllers/Organizer/events/PartisipantController.php
+++ app/Http/Controllers/Organizer/events/PartisipantController.php
@@ -18,9 +18,18 @@ class PartisipantController extends Controller
     
     public function acceptRequest(Event $event, $user)
     {
+        if($event->available_seats == 0){
+            return redirect()->back()->with('error', 'No seats available');
+        }
         $event->available_seats = $event->available_seats - 1;
         $event->users()->updateExistingPivot($user, ['status' => 'accepted']);
         $event->save();
-        return redirect()->back();
+        return redirect()->back()->with('success', 'Request accepted');
+    }
+
+    public function denieRequest(Event $event, $user)
+    {
+        $event->users()->updateExistingPivot($user, ['status' => 'denied']);
+        return redirect()->back()->with('success', 'Request denied');
     }
 }
